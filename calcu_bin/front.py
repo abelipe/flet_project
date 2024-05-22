5ad61a421f1fbb8eaa01ab6484c1b09725aac415
abelipe <157862231+abelipe@users.noreply.github.com>
2024-05-22 15:26:41 -0400

    btn regresar
    boton de regresar

--- calcu_bin/front.py
+++ calcu_bin/front.py
@@ -6,11 +6,17 @@ from gauss import Gausito, ElpanitaGauss
 def main(page: Page):
     def si(event):
         page.clean()
+        page.add(btn_r)
         traductor(page)
     def si2(event):
         page.clean()
+        page.add(btn_r)
         Gausito(page)
-        
+    
+    def regresar(event):
+        page.clean()
+        page.add(btn_r, fila_boton_click, Row(alignment=ft.MainAxisAlignment.CENTER, width=70, height=80) , fila_boton_click2)    
+    
     
     page.bgcolor = "#009999"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -23,5 +29,8 @@ def main(page: Page):
     fila_boton_click2 = Row([click2])
     fila_boton_click2.alignment = 'center'
     
-    page.add(fila_boton_click, Row(alignment=ft.MainAxisAlignment.CENTER, width=70, height=80) , fila_boton_click2)
+    btn_regresar = ElevatedButton(text = 'regresar', scale=2, on_click=regresar)
+    btn_r = Row([btn_regresar])
+    btn_r.alignment = 'start'
+    page.add(btn_r, fila_boton_click, Row(alignment=ft.MainAxisAlignment.CENTER, width=70, height=80) , fila_boton_click2)
 ft.app(target=main)
